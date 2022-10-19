f975414b5d4d51680b9bc854273b494e52f53a8b
KavyaCOder1 <97831532+KavyaCOder1@users.noreply.github.com>
2022-10-19 17:00:37 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,28 @@
+module.exports = {
+  token:'5751167071:AAEFjJoWMxEp09Ot_NRseTdZSk3soFC25_A', // token from @BotFather
+  mongoLink: 'mongodb://kavya9109:kavya9109@ac-f7sytui-shard-00-00.vajav6l.mongodb.net:27017,ac-f7sytui-shard-00-01.vajav6l.mongodb.net:27017,ac-f7sytui-shard-00-02.vajav6l.mongodb.net:27017/?ssl=true&replicaSet=atlas-5z6f25-shard-0&authSource=admin&retryWrites=true&w=majority', // mongo-link from cloud.mongodb.com
+  admin: '5556717138', // admin`s id for sending him/her errors,
+  currency:'PolyDoge',
+  min_wd:300000,// minimum withdrawal amount
+  refer_bonus:100000,
+  joinbal: 50000,
+  max_wd:5000000,
+  payment_channel:'@PaymentHub9109',
+  channel: '*👋 Hello ~ Complete Telegram Task:*\n\n🔹 Join *Airdrop Launcher* [Channel](https://t.me/AirdropLauncher9109) *And* [Group](t.me/airdroplauncherchat)\n🔹 Join *Bot Blaster* [Channel](https://t.me/BotBlaster) \n🔹 Join *Tricky Airdrops* [Channel](t.me/trickyAirdrops) \n🔹 Join *AT_DropsPayments* [Channel](t.me/at_dropspayments) \n🔹 Join *AirdropXOfficer* [Channel](t.me/AirdropXOfficer)  \n🔹 Join *Airdrop Frame* [Channel](t.me/airdropframe) \n🔹 Join *Verifieddropsi* [Channel](t.me/verifieddropsi)  \n🔹 Join *Payout Channel* [Payout](t.me/PaymentHub9109) on Telegram \n*🚊 Click "Check" button if done...*',
+  welcome:'*🔰 Airdrop Launcher Giveaway *\n*🎁 Reward Pool: 5,000,000,000 $PolyDoge*\n*👥 Referral: 500000 $PolyDoge*\n*🔥 Market: MEXC , QuickSwap*\n*📛 Winners:* For all valid users \n*🧶 Rate: ⭐️⭐️⭐️⭐️⭐️*\n*🚀 Distribution Date:* Instant to Wallet \n\n*📚 All the Tasks are Mandatory* \n     *🔹 @AirdropLauncher9109*\n     *🔹 @airdroplauncherchat*\n     *🔹 @botblaster*\n     *🔹 @trickyAirdrops*\n     *🔹 @AirdropXOfficer  *\n   *🔹 @airdropframe*\n *🔹 @Verifieddropsi*\n\n*♦️ @PaymentHub9109 (—payout—)*\n\n*💧 Contract:* `0xb4228798fF437ecD8fa43429664e9992256fe6Ac`\n\n*📡 Website: soon...*',
+  details:'*▪️ Network:* Polygon\n*▪️ Contract Address:* `0x8A953CfE442c5E8855cc6c61b1293FA648BAE472`\n*▪️ Name:* PolyDoge\n*▪️ Symbol:* PolyDoge\n*▪️ Decimals:* 18\n\n*📊 Market:* QuickSwap, MEXC',
+  welcomepic: 'https://te.legra.ph/file/25c91a93afbedf55fa371.png',
+  media: '*👋 Hello ~ Complete YouTube Task:*\n\n🔹 Follow *Airdrop Launcher* on [YouTube Channel](https://www.youtube.com/channel/UCxpjvrz7QtUUYgu5iuO4B1A) & Like and Comment `Good` on All Videos \n\n *📝 Submit your YouTube Link..... *',
+  addtext: '*💧 Submit Your Polygon (MATIC) address*\n\n‼️ *You can find it on Trust Wallet *(Do not use any exchange Address)',
+  addimage: 'https://te.legra.ph/file/edfba3a641ccab7d72e70.jpg',
+  symbol:'PolyDoge',
+  comment: '*👋Hello ~ Complete Optional Task:*\n\n🔹 Join Our *Promoters Telegram* [Channel1](https://t.me/AirdropLauncher9109) , [Channel2](t.me/AirdropManager9) , [Channel3](t.me/airdroplauncherchat)\n\n✅ And Click *Done☑️* Or You Can *Skip ⏭️* Also',
+  bot_username:'@PolyDoge_AutoV1_bot',
+  channels: ['@AirdropLauncher9109'],
+  channel1: '@AirdropXOfficer',
+  channel2: '@BotBlaster',
+  channel3: '@at_dropspayments',
+  channel4: '@verifieddropsi',
+  channel5: '@PaymentHub9109',
+}
+
